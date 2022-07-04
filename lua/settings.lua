9c7e4cf70000e1575f7e7c63fd0681df0648f1bb
xqft <estefano.bargas@fing.edu.uy>
2022-07-04 20:12:47 -0300

    Organized and added settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,24 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 0
+local opt = vim.opt
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Line number
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 4
+
+-- Text
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 0
+
+opt.wrap = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Other
+opt.backspace = 'indent,eol,start'
+opt.completeopt = 'menu,menuone,noselect'
 
 -- Highlights
 local highight = vim.api.nvim_set_hl
