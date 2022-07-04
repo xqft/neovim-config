8962c26e975087e9cc732946835aef068d1d2c6a
xqft <estefano.bargas@fing.edu.uy>
2022-07-03 23:01:51 -0300

    Basic config

---
+++ lua/plugins.lua
@@ -0,0 +1,8 @@
+require 'paq'
+{
+				'savq/paq-nvim';
+				'glacambre/firenvim';
+				'lervag/vimtex';
+				'tiagovla/tokyodark.nvim';
+				{'catppuccin/nvim', as='catppuccin'};
+}
---
+++ lua/settings.lua
@@ -0,0 +1,11 @@
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 0
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Highlights
+local highight = vim.api.nvim_set_hl
+
+
