2eaba5e4d8ebccbaa82e8176e3e0fefd39711e6a
xqft <estefano.bargas@fing.edu.uy>
2022-10-08 14:49:01 -0300

    Fixed colorscheme

--- lua/colors.lua
+++ lua/colors.lua
@@ -35,44 +35,21 @@ settings =
 				information = "underline",
 			},
 		},
-			coc_nvim = false,
-		lsp_trouble = false,
 		cmp = true,
-		lsp_saga = false,
-		gitgutter = false,
-		gitsigns = true,
-		telescope = true,
 		nvimtree = {
 			enabled = true,
 			show_root = false,
 			transparent_panel = false,
 		},
-		neotree = {
-			enabled = false,
-			show_root = false,
-			transparent_panel = false,
-		},
-		which_key = false,
 		indent_blankline = {
 			enabled = true,
 			colored_indent_levels = false,
 		},
 		dashboard = true,
-		neogit = false,
-		vim_sneak = false,
-		fern = false,
-		barbar = false,
-		bufferline = true,
 		markdown = true,
-		lightspeed = false,
-		ts_rainbow = false,
-		hop = false,
-		notify = true,
-		telekasten = true,
-		symbols_outline = true,
 	}
 }
 
-vim.g.catppuccin_flavour = 'frappe'
-catppuccin.setup(settings)
-vim.cmd[[colorscheme tokyodark]]
+vim.g.catppuccin_flavour = 'mocha'
+catppuccin.setup()
+vim.cmd[[colorscheme catppuccin]]
